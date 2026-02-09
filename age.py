b05a6bbdd461698f274d55690e7633da60ed4b98
Talha1337 <talhajaved@hotmail.co.uk>
2026-02-09 11:50:50 +0000

    added date checking

--- age.py
+++ age.py
@@ -1,17 +1,77 @@
 import datetime
 
 
-def age(date):
+def get_current_time():
+    return datetime.datetime.today()
+
+
+def age(date: str) -> int:
     """
     Docstring for age
 
     :param date: string in form DD-MM-YYYY
     """
-    current_date = datetime.datetime.today()
+    current_date = get_current_time()
     dt_date = datetime.datetime.strptime(date, "%d-%m-%Y")
 
     return int((current_date - dt_date).days/365)
 
 
+def check_date(date: str) -> bool:
+    """
+    Checks for if date string given is in valid format
+
+    :param date: string for date
+    """
+    date_components = date.split("-")
+    # splitting the months to check
+    # correct day count in comparison to the month
+
+    short_months = [2, 4, 7, 9, 11]
+    february = 2
+    if len(date_components) != 3:
+        print("3 components not found in date")
+        return False
+    day, month, year = date_components
+    print(day)
+    # ensuring all can be expressed as integers
+    try:
+        day = int(day)
+        print(f"day {day}")
+        month = int(month)
+        print(f"month {month}")
+        year = int(year)
+        print(f"year {year}")
+    except:
+        print("Integer conversion failed")
+        return False
+    if month > 12 or month < 1:
+        print("Month out of range")
+        return False
+    if day > 31 or day < 1:
+        print("Day out of range")
+        return False
+    # invalid month day combinations
+    if month in short_months and day > 30:
+        print("Day out of range given month")
+        return False
+    # leap years
+    if month == february:
+        if day > 29:
+            print("Day out of range given month")
+            return False
+        if day > 28 and year % 4:  # 29th but not a leap year
+            print("Day out of range given month and year")
+            return False
+    return True
+
+
 date = input("Please enter the date for calculating age (DD-MM-YYYY): ")
-print(f"The calculated age for {date} is {age(date=date)}")
+valid_date = check_date(date)
+if not valid_date:
+    print("Oh no, it looks like you might've put an invalid date in.")
+else:
+    calculated_age = age(date=date)
+    print(f"The calculated age for {date} is {age(date=date)}")
+    if calculated_age < 0:
+        print("This must be from the future.")
